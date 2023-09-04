2638a8306702acc2c562140b026aeeace58c0603
houwenchen <houwenchen_work@163.com>
2023-09-05 00:49:19 +0800

    init project
    Signed-off-by: houwenchen <houwenchen_work@163.com>

---
+++ pkg/running/running.go
@@ -0,0 +1,29 @@
+package running
+
+import (
+	"context"
+	"houwenchen/gorobot/pkg/parser"
+	"time"
+)
+
+// 整个工具的对外接口
+// 将各个小接口集成进来
+type Runner interface {
+	parser.Parser
+
+	Run() error
+}
+
+type runner struct {
+}
+
+func (r *runner) Run() error {
+	return nil
+}
+
+func (r *runner) setup() (context.Context, context.CancelFunc) {
+	ctx := context.TODO()
+	timeout := 100 * time.Minute
+
+	return context.WithTimeout(ctx, timeout)
+}
