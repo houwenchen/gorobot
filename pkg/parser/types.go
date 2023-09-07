b63eba893ff45295cd858374509d95a943cc07e5
houwenchen <houwenchen_work@163.com>
2023-09-07 17:10:27 +0800

    init project
    Signed-off-by: houwenchen <houwenchen_work@163.com>

--- pkg/parser/types.go
+++ pkg/parser/types.go
@@ -1,6 +1,33 @@
 package parser
 
+import "time"
+
 const (
 	// 文件扩展名
 	Extention string = ".gorobot"
 )
+
+type Settings struct {
+	Document string
+	Timeout  time.Duration
+}
+
+type Packages struct {
+	Library []string
+}
+
+type Variables struct {
+	Variable map[string]interface{}
+}
+
+type TestCases struct {
+}
+
+type TestCase struct {
+}
+
+type Keywords struct {
+}
+
+type Keyword struct {
+}
